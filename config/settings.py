2a60d45c7ca6a8e29913ec3454e077368063cfdd
Diego Ayala <dieguito9677@gmail.com>
2026-01-15 00:33:46 -0400

    fix: Import os module to resolve NameError in settings.py

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-&an#zmd_y+u5l_&^a0+-4ilu!b^q7_8ud-)pq&=f(f2m^gb-!p')
